719c71d9b4f20ae59a629178ca93473fad980d00
sho-kasama <sho-kasama@trunk.fm>
2019-06-06 14:52:23 +0900

    add

--- todo/src/Form.js
+++ todo/src/Form.js
@@ -1,25 +1,17 @@
-import React, { Component } from 'react'
-import './css/form.css'
+import React, { Component } from 'react';
+import './css/form.css';
 
-class Form extends Component {
+export default class Form extends Component {
+
     render() {
         return (
             <div className="form">
-                <form>
-                    <input name="title" type="text" placeholder="タイトル *必須やで" defaultValue="reactの勉強" /><br />
-                    <textarea name="desc" placeholder="説明を入力" defaultValue="todoアプリを作っています!"></textarea><br />
+                <form name="todoform" onSubmit={this.props.onSubmit}>
+                    <input name="title" type="text" placeholder="タイトル ※必須" defaultValue="reactの勉強" /><br />
+                    <textarea name="desc" placeholder="説明を入力" defaultValue="todoアプリを作っています！"></textarea><br />
                     <button type="submit">todoを作成</button>
                 </form>
             </div>
-        )
+        );
     }
-}
-
-export default Form
-
-{
-
-    // Formタグを使って通常のHTML同様のフォームを作る
-    // 入力するのが面倒ならdefaultValueを作成する
-
 }
